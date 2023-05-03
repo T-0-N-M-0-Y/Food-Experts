9694e4af278c05317133f6d629a9ab68a76b178a
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-05-04 01:38:13 +0600

    Spinner

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,20 +1,26 @@
 import React, { useContext } from 'react';
 import { AuthContext } from './AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
-const PrivateRoute = ({children}) => {
+const PrivateRoute = ({ children }) => {
 
-    const {user, loading} = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
     const location = useLocation();
 
-    if(loading){
-        return
+    if (loading) {
+        return (
+            <div>
+                <span className='flex justify-center text-4xl my-20'><FontAwesomeIcon icon={faSpinner} className='animate-spin'></FontAwesomeIcon></span>
+            </div>
+        )
     }
 
-    if(user){
+    if (user) {
         return children;
     }
-    return <Navigate state={{from: location}} to={"/login"} replace></Navigate>
+    return <Navigate state={{ from: location }} to={"/login"} replace></Navigate>
 };
 
 export default PrivateRoute;
